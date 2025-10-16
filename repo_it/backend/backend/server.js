4909efef68c5bfabf8dbee21092ee7c313410443
antoninomazzaglia8 <antoninomazzaglia8@gmail.com>
2025-10-17 00:22:01 +0200

    Delete repo_it/backend/backend directory

--- repo_it/backend/backend/server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-app.use(express.json());
-app.get('/api/ping', (req,res)=>res.json({msg:'pong'}));
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>console.log('Backend running'));
