784edba5d87fa7c72744acd744c31e79bac74af7
antoninomazzaglia8 <antoninomazzaglia8@gmail.com>
2025-10-17 16:07:04 +0200

    Update server.js

--- repo_it/backend/server.js
+++ repo_it/backend/server.js
@@ -4,4 +4,4 @@ const app = express();
 app.use(express.json());
 app.get('/api/ping', (req,res)=>res.json({msg:'pong'}));
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>console.log('Backend running'));
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
